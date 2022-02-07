17028a0cdeb9913759e25a17935c4be3bbfb88a1
dwidecki <dylan.widecki@quinnipiac.edu>
2022-02-07 18:02:00 -0500

    asked user for two numbers to add or subtract based on user input

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,7 +7,15 @@ public class Calculator {
 		int operand1 = sc.nextInt();
 		System.out.println("Enter in a number");
 		int operand2 = sc.nextInt();
-		System.out.println((operand1 + operand2));
+		System.out.println("Press 1 to add, Press 2 to subtract");
+		int answer = sc.nextInt();
+		if(answer == 1) {
+			System.out.println((operand1 + operand2));
+		}
+		else {
+			System.out.println((operand1 - operand2));	
+		}
+		
 	}
 
 }
