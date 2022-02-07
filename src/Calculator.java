3add31a6800c6983d5128be5be3097d381292d6f
dwidecki <dylan.widecki@quinnipiac.edu>
2022-02-07 16:50:46 -0500

    asked user to enter two numbers in order to print the sum

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 
 public class Calculator {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter in a number");
+		int operand1 = sc.nextInt();
+		System.out.println("Enter in a number");
+		int operand2 = sc.nextInt();
+		System.out.println((operand1 + operand2));
 	}
 
 }
