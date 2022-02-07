79a32869350ef3ead64c2df1a5853ce3803459f5
dwidecki <dylan.widecki@quinnipiac.edu>
2022-02-07 18:13:33 -0500

    asked user for two numbers to add, subtract, multiply or divide based on user input

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,13 +7,19 @@ public class Calculator {
 		int operand1 = sc.nextInt();
 		System.out.println("Enter in a number");
 		int operand2 = sc.nextInt();
-		System.out.println("Press 1 to add, Press 2 to subtract");
+		System.out.println("Press 1 to add, Press 2 to subtract, Press 3 to multiply, Press 4 to divide");
 		int answer = sc.nextInt();
 		if(answer == 1) {
 			System.out.println((operand1 + operand2));
 		}
-		else {
+		else if(answer == 2) {
 			System.out.println((operand1 - operand2));	
+		}
+		else if(answer == 3) {
+			System.out.println((operand1 * operand2));
+		}
+		else {
+			System.out.println((operand1 / operand2));
 		}
 		
 	}
